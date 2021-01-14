bf9576c036bc1abf45c4a784930f05ffbf7ca16b
dev-enes <77413333+dev-enes@users.noreply.github.com>
2021-01-14 04:10:49 +0300

    delete files

--- main-js/contact.js
+++
@@ -1,9 +0,0 @@
-var name = document.getElementById('name');
-var subject = document.getElementById('subject');
-var email = document.getElementById('email');
-var msg = document.getElementById('msg');
-
-
-function alertUser() {
-    alert("Feature not fully implemented.");
-}
