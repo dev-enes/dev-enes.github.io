4783086d70744085fbb772b6e8efd2bce1c71554
dev-enes <77413333+dev-enes@users.noreply.github.com>
2021-01-14 04:11:10 +0300

    upload files

---
+++ main-js/contact.js
@@ -0,0 +1,9 @@
+var name = document.getElementById('name');
+var subject = document.getElementById('subject');
+var email = document.getElementById('email');
+var msg = document.getElementById('msg');
+
+
+function alertUser() {
+    alert("Feature not fully implemented.");
+}
